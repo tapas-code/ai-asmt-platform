6fb7c721dd0616b75ff2d8091b92d7e71eb2e4f1
tapas-code <tapas.code247@gmail.com>
2026-07-04 11:46:43 +0530

    refactor: remove OpenNext Cloudflare development initialization from next configuration

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-if (process.env.NODE_ENV !== "production") {
-  const { initOpenNextCloudflareForDev } = await import("@opennextjs/cloudflare");
-  initOpenNextCloudflareForDev();
-}
-
 export default nextConfig;
